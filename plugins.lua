7d36720132575c0c99752b752c3cd07261693387
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-09 22:45:30 +0000

    ensure tresitter modules installed

--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,21 @@
 local plugins = {
+  {"github/copilot.vim", lazy = false},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "bash"
+      },
+    },
+  },
 }
 
 return plugins
