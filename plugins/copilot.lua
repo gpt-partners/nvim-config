10d63537641a04f1f5e377962be98a3b30020765
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-13 22:32:13 +0000

    remove cmp for Copilot

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -21,18 +21,4 @@ return {
       },
     },
   },
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require "copilot_cmp"
-          copilot_cmp.setup(opts)
-        end,
-      },
-    },
-  },
 }
