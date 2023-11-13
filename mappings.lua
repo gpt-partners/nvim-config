40a75334bdb8a99bb97ca6a0666afb43d5fac626
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-13 13:02:19 +0000

    fix qa

--- mappings.lua
+++ mappings.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.general = {
   n = {
-    ["q"] = { "<cmd> qa! <CR>", "Quit" },
+    ["<leader>q"] = { "<cmd> qa! <CR>", "Quit" },
     [","] = { "<cmd> w <CR>", "Save file" },
     ["-"] = {
       function()
