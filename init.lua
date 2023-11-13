63d7e92cfda32114a8adddff7df9673f571ffd83
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-13 14:15:06 +0000

    add autotest support for pytest

--- init.lua
+++ init.lua
@@ -1,8 +1,17 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Format on save
 autocmd("BufWritePre", {
   pattern = "*",
   callback = function(args)
     require("conform").format { bufnr = args.buf }
+  end,
+})
+
+-- Run pytest on save
+autocmd("BufWritePost", {
+  pattern = "*.py",
+  callback = function()
+    require("neotest").run.run(vim.loop.cwd())
   end,
 })
