c4926b20c53f1d59f9762f50e07c220f6d8cc75f
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-22 19:47:45 +0000

    add ChatGPT plugin

--- plugins.lua
+++ plugins.lua
@@ -5,6 +5,18 @@ local plugins = {
     import = "custom.plugins.copilot",
   },
   {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  {
     import = "custom.plugins.neotest",
   },
   {
